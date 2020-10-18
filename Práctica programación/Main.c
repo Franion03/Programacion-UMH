f8faea4c41484bec633714aebb0581165511a1da
Franion03 <Franion03@users.noreply.github.com>
2020-10-19 01:20:27 +0200

    domingo

--- Práctica programación/Main.c
+++ Práctica programación/Main.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
+#include "lib.h"
+
 
 int main(int argc, char* argv[]){
-    printf("Hola Mundo");
+    int numTok;
+    char ptr[10000];
+    char comando[1000];
+
+    while(1){
+        gets(comando);
+        numTok=0;
+        ptr[numTok]=strtok (comando, " \n\t");
+        numTok++;
+        while( (ptr[numTok] = strtok( NULL, " \n\t")) != NULL)
+        {
+            numTok++;   
+        }
+    }
+    printf("FIN\n" );
     return 0;
 }
---
+++ Práctica programación/lib.h
@@ -0,0 +1,18 @@
+#ifndef _lib_H_
+#define _lib_H_
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
